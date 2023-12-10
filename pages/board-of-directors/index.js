22d177ec282eea43e6b51d5c7ad7c6a55f716b9d
Mamun13 <mamuncse82@gmail.com>
2023-12-10 10:54:48 +0600

    parse

--- pages/board-of-directors/index.js
+++ pages/board-of-directors/index.js
@@ -1,23 +1,13 @@
 import Image from "next/image";
 import Row from "react-bootstrap/Row";
 import Directors from "../../public/bod/Banner.png";
-// import Chairman from "../../public/bod/chairman.jpg";
-// import Chairman2 from "../../public/bod/chairman2.png";
-// import Chairman3 from "../../public/bod/chairman3.png";
-// import Chairman4 from "../../public/bod/chairman4.jpg";
-// import Chairman5 from "../../public/bod/chairman5.png";
-// import Leader from "../../public/bod/leader.jpg";
-// import TeamOne from "../../public/bod/teamone.jpg";
-// import TeamTwo from "../../public/bod/teamtwo.jpg";
-// import TeamThree from "../../public/bod/teamthree.jpg";
-// import TeamFour from "../../public/bod/teamfour.jpg";
-// import TeamSix from "../../public/bod/teamsix.png";
-// import TeamSeven from "../../public/bod/teamseven.jpg";
-// import TeamEight from "../../public/bod/teameight.jpg";
+
 import Head from "next/head";
 import { makeTitle } from "../../utils/helpers";
 import React, { Fragment, useEffect, useState } from "react";
 import axios from "../../utils/axios";
+import { getStoragePath } from "../../utils/helpers";
+import parse from "html-react-parser";
 
 const BoardOfDirectors = () => {
   const [info, setInfo] = useState([]);
@@ -66,28 +56,31 @@ const BoardOfDirectors = () => {
           <h1 className="bod-page-title text-capitalize font-40 fw-bold text-center py-5 font-lato">
             Corporate Office Organogram
           </h1>
-          
+
           {/* Chirman */}
           <div className="d-flex justify-content-center mb-5">
-            <div className="col-lg-4 col-md-4 mb-4">
-              <div className="text-center">
-                <img
-                  src={info[0]?.item_image}
-                  alt=""
-                  className="rounded leadership-team"
-                />
-                <h2 className="font-20 fw-bold pt-2 pb-1">
-                  {info[0]?.item_name}
-                </h2>
-                <p className="position-relative pt-1">
-                  {info[0]?.item_short_desc}
-                </p>
-              </div>
-            </div>
+            {info.map((curElem, key) => {
+              return (
+                <>
+                  <div className="col-lg-4 col-md-4 mb-4">
+                    <div className="text-center">
+                      <img
+                        src={getStoragePath(curElem.item_image)}
+                        alt=""
+                        className="rounded leadership-team"
+                      />
+                      <h2 className="font-20 fw-bold pt-2 pb-1">
+                        {curElem.item_name}
+                      </h2>
+                      <p className="position-relative pt-1">
+                        {curElem.item_short_desc}
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
-
-
-          
 
           {/* <div className="row d-flex justify-content-evenly">
             <div className="col-lg-4 col-md-4 mb-4">
@@ -138,7 +131,7 @@ const BoardOfDirectors = () => {
                     <div className="col-lg-4 col-md-4 mb-4">
                       <div className="text-center">
                         <img
-                          src={curElem.item_image}
+                          src={getStoragePath(curElem.item_image)}
                           alt=""
                           className="rounded-pill chairman-img"
                         />
@@ -146,7 +139,6 @@ const BoardOfDirectors = () => {
                           {curElem.item_name}
                         </h2>
                         <p className="about_titledesign position-relative pt-2">
-                          
                           {curElem.item_short_desc}
                         </p>
                       </div>
@@ -154,15 +146,12 @@ const BoardOfDirectors = () => {
                   </>
                 );
               })}
-
             </Row>
           </div>
 
           <h1 className="text-capitalize font-40 fw-bold text-center py-5 font-lato">
-            Marketing Team
+            Tele Sales
           </h1>
-         
-
 
           <div className="row">
             {member?.map((curElem, key) => {
@@ -171,7 +160,7 @@ const BoardOfDirectors = () => {
                   <div className="col-lg-3 col-md-3 col-sm-6 mb-4">
                     <div className="text-center">
                       <img
-                        src={curElem.item_image}
+                        src={getStoragePath(curElem.item_image)}
                         alt=""
                         className="rounded new-team-member"
                       />
@@ -179,7 +168,7 @@ const BoardOfDirectors = () => {
                         {curElem.item_name}
                       </h2>
                       <p className="position-relative pt-1">
-                        {curElem.item_short_desc}
+                        {parse(curElem.item_short_desc)}
                       </p>
                     </div>
                   </div>
