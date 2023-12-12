194a1af78305ebd80bd393278535f51376ca77fc
Mamun13 <mamuncse82@gmail.com>
2023-12-12 18:07:22 +0600

    update

--- pages/board-of-directors/index.js
+++ pages/board-of-directors/index.js
@@ -8,8 +8,12 @@ import React, { Fragment, useEffect, useState } from "react";
 import axios from "../../utils/axios";
 import { getStoragePath } from "../../utils/helpers";
 import parse from "html-react-parser";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const BoardOfDirectors = () => {
+  const router = useRouter();
+
   const [info, setInfo] = useState([]);
   const [raj_rong, setRaj_rong] = useState([]);
   const [bod, setBod] = useState([]);
@@ -28,8 +32,6 @@ const BoardOfDirectors = () => {
     fetchTeamLead();
   }, []);
 
-
-
   const fetchBoardofDirectors = () => {
     axios.get(`/content-module/1`).then((res) => {
       setBod(res?.data[0]?.content_item);
@@ -39,7 +41,7 @@ const BoardOfDirectors = () => {
     fetchBoardofDirectors();
   }, []);
 
-// Rajshahi and Rangpur Division
+  // Rajshahi and Rangpur Division
   const fetchMemberPerson = () => {
     axios.get(`/content-module/30`).then((res) => {
       setRaj_rong(res?.data[0]?.content_item);
@@ -49,46 +51,45 @@ const BoardOfDirectors = () => {
     fetchMemberPerson();
   }, []);
 
-// khulna and barisal 
-const fetchkhul_bari = () => {
-  axios.get(`/content-module/31`).then((res) => {
-    setKhul_bari(res?.data[0]?.content_item);
-  });
-};
-useEffect(() => {
-  fetchkhul_bari();
-}, []);
+  // khulna and barisal
+  const fetchkhul_bari = () => {
+    axios.get(`/content-module/31`).then((res) => {
+      setKhul_bari(res?.data[0]?.content_item);
+    });
+  };
+  useEffect(() => {
+    fetchkhul_bari();
+  }, []);
 
-// Mymensingh  and dhaka 
-const fetchMymDha = () => {
-  axios.get(`/content-module/32`).then((res) => {
-    setMymdha(res?.data[0]?.content_item);
-  });
-};
-useEffect(() => {
-  fetchMymDha();
-}, []);
+  // Mymensingh  and dhaka
+  const fetchMymDha = () => {
+    axios.get(`/content-module/32`).then((res) => {
+      setMymdha(res?.data[0]?.content_item);
+    });
+  };
+  useEffect(() => {
+    fetchMymDha();
+  }, []);
 
-// Dhaka Part-1 and Dhaka Part-2 Division
-const fetchDhaDha = () => {
-  axios.get(`/content-module/33`).then((res) => {
-    setDhadha(res?.data[0]?.content_item);
-  });
-};
-useEffect(() => {
-  fetchDhaDha();
-}, []);
+  // Dhaka Part-1 and Dhaka Part-2 Division
+  const fetchDhaDha = () => {
+    axios.get(`/content-module/33`).then((res) => {
+      setDhadha(res?.data[0]?.content_item);
+    });
+  };
+  useEffect(() => {
+    fetchDhaDha();
+  }, []);
 
-// Chattogram and Sylhet division
-const fetchChat_Syl = () => {
-  axios.get(`/content-module/34`).then((res) => {
-    setChat_Syl(res?.data[0]?.content_item);
-  });
-};
-useEffect(() => {
-  fetchChat_Syl();
-}, []);
-
+  // Chattogram and Sylhet division
+  const fetchChat_Syl = () => {
+    axios.get(`/content-module/34`).then((res) => {
+      setChat_Syl(res?.data[0]?.content_item);
+    });
+  };
+  useEffect(() => {
+    fetchChat_Syl();
+  }, []);
 
   return (
     <Fragment>
@@ -111,11 +112,13 @@ useEffect(() => {
                 <>
                   <div className="col-lg-4 col-md-4 mb-4">
                     <div className="text-center">
-                      <img
-                        src={getStoragePath(curElem.item_image)}
-                        alt=""
-                        className="rounded leadership-team"
-                      />
+                      <Link href={`/comment/${curElem.id}`}>
+                        <img
+                          src={getStoragePath(curElem.item_image)}
+                          alt=""
+                          className="rounded leadership-team"
+                        />
+                      </Link>
                       <h2 className="font-20 fw-bold pt-2 pb-1">
                         {curElem.item_name}
                       </h2>
@@ -159,15 +162,17 @@ useEffect(() => {
             </Row>
           </div>
 
+          
+          {/* marketing part */}
           <div className=" text-center">
             <span className="text-capitalize font-40 fw-bold my-5 marketing_team position-relative">
               Marketing Team
             </span>
           </div>
 
-          <div className="row">
+          <div className="row d-flex justify-content-center ">
             <p className="text-capitalize fw-bold py-4 font-24 text-center">
-            Rajshahi and Rangpur Division
+              Rajshahi and Rangpur Division
             </p>
             {raj_rong?.map((curElem, key) => {
               return (
@@ -192,9 +197,9 @@ useEffect(() => {
               );
             })}
           </div>
-          <div className="row">
+          <div className="row d-flex justify-content-center ">
             <p className="text-capitalize fw-bold py-4 font-24 text-center">
-            Khulna and Barisal Division
+              Khulna and Barisal Division
             </p>
             {khul_bari?.map((curElem, key) => {
               return (
@@ -219,9 +224,9 @@ useEffect(() => {
               );
             })}
           </div>
-          <div className="row">
+          <div className="row d-flex justify-content-center ">
             <p className="text-capitalize fw-bold py-4 font-24 text-center">
-            Mymensingh and Dhaka Part-3 Division
+              Mymensingh and Dhaka Part-3 Division
             </p>
             {mymdha?.map((curElem, key) => {
               return (
@@ -246,9 +251,9 @@ useEffect(() => {
               );
             })}
           </div>
-          <div className="row">
+          <div className="row d-flex justify-content-center ">
             <p className="text-capitalize fw-bold py-4 font-24 text-center">
-            Dhaka Part-1 and Dhaka Part-2 Division
+              Dhaka Part-1 and Dhaka Part-2 Division
             </p>
             {dhadha?.map((curElem, key) => {
               return (
@@ -273,10 +278,9 @@ useEffect(() => {
               );
             })}
           </div>
-
-          <div className="row">
+          <div className="row d-flex justify-content-center">
             <p className="text-capitalize fw-bold py-4 font-24 text-center">
-            Chattogram and Sylhet division
+              Chattogram and Sylhet division
             </p>
             {cha_syl?.map((curElem, key) => {
               return (
